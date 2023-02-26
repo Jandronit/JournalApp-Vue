2b9bdfae5a534e40e2ee3e74913882046c8c4bb9
margueellohenriquez <maarguello@arsys.es>
2023-02-26 21:08:27 +0100

    (lesson 180 course vue) Add Commit a mutation for VUEX

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,24 +1,5 @@
 
 export default () =>  ({
     isLoading: true,
-    entries: [
-        {
-            id: '1',
-            date: new Date().toDateString(),
-            text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-            picture: null,
-        },
-        {
-            id: '2',
-            date: new Date().toDateString(),
-            text: 'Voluptate culpa quia, quod, quas, voluptatum quibusdam quidem voluptates quae quos voluptas quas.',
-            picture: null,
-        },
-        {
-            id: '3',
-            date: new Date().toDateString(),
-            text: 'Fugiat consequuntur, quod, quas, voluptatum quibusdam quidem voluptates quae quos voluptas quas.',
-            picture: null,
-        }
-    ]
+    entries: []
 })
