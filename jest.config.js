a049f382ee91832e6fb9b4973039df8c34c3058d
Jandronit <jandrohen@gmail.com>
2023-03-26 19:11:39 +0200

    (lesson 200 course vue) Test Verify that routes load expected components

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  // Agrega transformación personalizada
+  transformIgnorePatterns: ['/node_modules/(?!axios)'],
 }
--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -1,22 +1,30 @@
 import daybookRouter from '@/modules/daybook/router'
 describe('Test in router module to Daybook', () => {
-  test('the router must have this configuration', () => {
-    expect(daybookRouter).toMatchObject({
-      name: 'daybook',
-      component: expect.any(Function),
-      children: [
-        {
-            path: '',
-            name: 'no-entry',
-            component: expect.any(Function)
-        },
-        {
-            path: ':id',
-            name: 'entry',
+    test('the router must have this configuration', async () => {
+        expect(daybookRouter).toMatchObject({
+            name: 'daybook',
             component: expect.any(Function),
-            props: expect.any(Function)
-        }
-      ]
+            children: [
+                {
+                    path: '',
+                    name: 'no-entry',
+                    component: expect.any(Function)
+                },
+                {
+                    path: ':id',
+                    name: 'entry',
+                    component: expect.any(Function),
+                    props: expect.any(Function)
+                }
+            ]
+        })
+        const promiseRoutes = []
+        daybookRouter.children.forEach(child => promiseRoutes.push(child.component()))
+
+        const routes = (await Promise.all(promiseRoutes)).map(promise => promise.default.name)
+
+        expect(routes).toContain('NoEntrySelected')
+        expect(routes).toContain('EntryView')
+
     })
-  })
 })
