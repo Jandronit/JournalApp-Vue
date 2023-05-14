d7b135b1b7005039639ab7e9ce8a725631a86140
Jandronit <jandrohen@gmail.com>
2023-05-14 14:13:22 +0200

    (lesson 275 course vue) Add Interceptors

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -1,7 +1,13 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const journalApi = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
+  baseURL: process.env.VUE_APP_API_URL,
+});
+journalApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem("idToken");
+  if (token) {
+    config.params = { auth: token };
+  }
+  return config;
 });
 export default journalApi;
