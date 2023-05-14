0a0cb3453d050d9434c76d4b2696a67dfea28bac
Jandronit <jandrohen@gmail.com>
2023-05-14 19:07:09 +0200

    (lesson 282 course vue)  Mutations - loginUser

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -11,4 +11,26 @@ describe("Vuex - Test in the Auth Module", () => {
     expect(idToken).toBe(null);
     expect(refreshToken).toBe(null);
   });
+
+  // Mutations
+  test("mutations: loginUser", () => {
+    const store = createVuexStore(initialStateFake);
+    const payload = {
+      user: {
+        name: "Test",
+        email: "test@test.es",
+      },
+      idToken: "ABC-123",
+      refreshToken: "XYZ-123",
+    };
+
+    store.commit("auth/loginUser", payload);
+
+    const { status, user, idToken, refreshToken } = store.state.auth;
+
+    expect(status).toBe("authenticated");
+    expect(user).toEqual(payload.user);
+    expect(idToken).toBe(payload.idToken);
+    expect(refreshToken).toBe(payload.refreshToken);
+  });
 });
