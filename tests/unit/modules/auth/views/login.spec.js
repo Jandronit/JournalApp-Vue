570c34c962411c6d39afa252a57471bcc5f889e4
Jandronit <jandrohen@gmail.com>
2023-05-26 22:13:46 +0200

    (lesson 299 course vue) * Tests correct form

--- tests/unit/modules/auth/views/login.spec.js
+++ tests/unit/modules/auth/views/login.spec.js
@@ -4,7 +4,9 @@ import createVuexStore from "../../../../mocks/mock-store";
 import { useRouter } from "vue-router";
 import Swal from "sweetalert2";
 jest.mock("vue-router", () => ({
-  useRouter: jest.fn(),
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
 }));
 jest.mock("sweetalert2", () => ({
   fire: jest.fn(),
@@ -34,7 +36,7 @@ describe("Tests Login Component", () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  test(" incorrect credentials, dispatch the SWAL ", async () => {
+  test("incorrect credentials, dispatch the SWAL ", async () => {
     store.dispatch.mockReturnValueOnce({
       ok: false,
       message: "Bad credentials",
@@ -57,5 +59,33 @@ describe("Tests Login Component", () => {
     });
 
     expect(Swal.fire).toHaveBeenCalledWith("Error", "Bad credentials", "error");
+  });
+
+  test("correct credentials, should be redirect to route no-entry", async () => {
+    store.dispatch.mockReturnValueOnce({
+      ok: true,
+    });
+
+    const wrapper = shallowMount(Login, {
+      global: {
+        plugins: [store],
+        mocks: {
+          $router: useRouter(),
+        },
+      },
+    });
+
+    const [txtEmail, txtPassword] = wrapper.findAll("input");
+    await txtEmail.setValue("test@test.es");
+    await txtPassword.setValue("123456");
+
+    await wrapper.find("form").trigger("submit.prevent");
+
+    expect(store.dispatch).toHaveBeenCalledWith("auth/signInUser", {
+      email: "test@test.es",
+      password: "123456",
+    });
+
+    expect(Swal.fire).not.toHaveBeenCalled();
   });
 });
